7d05c203ff4a88ca758391984be44ef7b96ce9d1
Ulyana <ubrataus@gmail.com>
2022-04-28 17:10:29 +0300

    mainDash section finished

--- src/components/MainDash.jsx/MainDash.jsx
+++ src/components/MainDash.jsx/MainDash.jsx
@@ -1,12 +1,15 @@
 import "./MainDash.css";
 import React from "react";
 import Cards from "../Cards/Cards";
+import Table from "../Table/Table";
 
 const MainDash = () => {
   return (
     <section className="main-dash">
       <h1>Dashboard</h1>
       <Cards />
+
+      <Table />
     </section>
   );
 };
